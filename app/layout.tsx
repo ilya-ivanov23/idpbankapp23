b7a15259b1b2bca0ae7e953176b4eb66bb80e2e1
ilya-ivanov23 <ilya.nemogay@gmail.com>
2025-08-20 14:07:39 +0200

    base commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,32 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-});
-
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
-  variable: '--font-ibm-plex-serif',
-    weight: ['400', '700'],
   subsets: ['latin'],
-});
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
   title: "IDPBankApp",
   description: "IDP multiplatform for user which chooses future",
-    icons: {
-      icon: '/icons/logo.svg'
-    }
+  icons: {
+    icon: '/icons/logo.svg'
+  }
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${ibmPlexSerif.variable} `}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+      </html>
   );
 }
